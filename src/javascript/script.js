80e8b50bd026895503811e1d28aa4f48c193e980
Jaqueline <jaqueline_fanton@estudante.sesisenai.org.br>
2026-05-05 16:49:54 -0300

    J

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,37 +1,50 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $('#mobile_btn').on('click', function() {
+    // MENU MOBILE
+    $('#mobile_btn').on('click', function () {
         $('#mobile_menu').toggleClass('active');
-        $('#mobile_btn').find('i').toggleClass('fa-x');
+        $('#mobile_btn i').toggleClass('fa-x');
     });
 
+    // NAV ACTIVE
     const sections = $('section');
-    const navItemsDesktop = $('#nav_list .nav-item');
-    const navItemsMobile = $('#mobile_nav_list .nav-item');
+    const navItems = $('#nav_list .nav-item');
 
     $(window).on('scroll', function () {
+        const scroll = $(window).scrollTop();
 
-        const header = $('header');
-        const scrollPosition = $(window).scrollTop() - header.outerHeight();
-
-        let activeSectionIndex = 0;
-
-        sections.each(function(i) {
-            const sectionTop = $(this).offset().top - header.outerHeight();
-            const sectionBottom = sectionTop + $(this).outerHeight();
-
-            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-                activeSectionIndex = i;
-                return false;
+        sections.each(function (i) {
+            if (scroll >= $(this).offset().top - 100) {
+                navItems.removeClass('active');
+                $(navItems[i]).addClass('active');
             }
         });
-
-        // remove de todos
-        navItemsDesktop.removeClass('active');
-        navItemsMobile.removeClass('active');
-
-        // adiciona nos dois menus
-        $(navItemsDesktop[activeSectionIndex]).addClass('active');
-        $(navItemsMobile[activeSectionIndex]).addClass('active');
     });
+
+    // CONTADORES
+    let likes = 0;
+    let cart = 0;
+
+    // CURTIR ❤️
+    $('.collection-heart, .fashion-heart, .jewel-heart').click(function () {
+
+        const icon = $(this).find('i');
+
+        if (!icon.hasClass('liked')) {
+            icon.addClass('liked');
+            likes++;
+        } else {
+            icon.removeClass('liked');
+            likes--;
+        }
+
+        $('#like_count').text(likes);
+    });
+
+    // CARRINHO 🛒
+    $('.btn-default').click(function () {
+        cart++;
+        $('#cart_count').text(cart);
+    });
+
 });
